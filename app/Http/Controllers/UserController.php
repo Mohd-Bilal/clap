28fa4dd2664f4a70caf4c6952d3c63c4134cde90
albin <padinju@gmail.com>
2018-01-16 17:57:29 +0530

    signup

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,90 +13,110 @@ use Socialite;
 
 class UserController extends Controller{
 
-    public function getDashboard(){
+  public function getDashboard(){
 
-      return view('dashboard');
+    return view('dashboard');
 
-      }
-
-	
-
-    public function Signup(Request $request){
-        $this->validate($request,[
-          'email' => 'required|email',
-          'first_name' => 'required|max:120',
-          'last_name' => 'required|max:120',
-          'username'=>'required',
-          'password' => 'required|min:8',
-          'confirmpassword'=>'required_with:password|same:password|min:8',
-          'gender' => 'required',
-          'channel' => 'required',
-
-          ]);
-        $password=bcrypt($request['password']);
-        $interest=['i1'=>$request['i1']?1:-1,'i2'=>$request['i2']?1:-1,'i3'=>$request['i3']?1:-1,'i4'=>$request['i4']?1:-1,'i5'=>$request['i5']?1:-1];
-	if ($request['gender']=='M')
-          		$avatar='/src/img/dummymale.jpg';
-      	else
-        		$avatar='/src/img/dummyfemale.jpeg';       
-
-
-        $client = new Client(); //GuzzleHttp\Client
-	$result = $client->post('http://localhost:3000/authentication/signup/renegade', [
-    		'form_params' => [
-        		'username' => $request['username'],
-			'email' => $request['email'],
-			'password' => $request['password'],
-			'first_name' =>$request['first_name'],
-			'second_name' =>$request['last_name'],
-			'gender' => $request['gender'],
-			'occupation' => $request['channel'],
-			'avatar' => $avatar
-		   
-    		]
-	]);    
-
-        return("data send");      
+  }
 
 
 
+  public function Signup(Request $request){
+    $this->validate($request,[
+      'email' => 'required|email',
+      'first_name' => 'required|max:120',
+      'last_name' => 'required|max:120',
+      'username'=>'required',
+      'password' => 'required|min:8',
+      'confirmpassword'=>'required_with:password|same:password|min:8',
+      'gender' => 'required',
+      'channel' => 'required',
 
-        
-      
-        return redirect()->route('dashboard');
+    ]);
 
-    }
-    public function isOn($interest){
-      if($interest=='on')
-        return 1;
-      else
-        return 0;
-    }
-    public function SignIn(Request $request)
-    {
-
-      $this->validate($request,[
-      	'username' => 'required',
-      	'password' => 'required',
-
-      	]);
+    $interest=['i1'=>$request['i1']?1:-1,'i2'=>$request['i2']?1:-1,'i3'=>$request['i3']?1:-1,'i4'=>$request['i4']?1:-1,'i5'=>$request['i5']?1:-1];
+    if ($request['gender']=='M')
+    $avatar='/src/img/dummymale.jpg';
+    else
+    $avatar='/src/img/dummyfemale.jpeg';
 
 
-	    if (Auth::attempt(['username'=> $request['username'],'password'=> $request['password']])){
+    $client = new Client(); //GuzzleHttp\Client
+    $body = $client->post('http://localhost:3000/authentication/signup/renegade', [
+      'form_params' => [
+        'username' => $request['username'],
+        'email' => $request['email'],
+        'password' => $request['password'],
+        'first_name' =>$request['first_name'],
+        'second_name' =>$request['last_name'],
+        'gender' => $request['gender'],
+        'occupation' => $request['channel'],
+        'avatar' => $avatar
 
-		    return redirect()->route('dashboard');
+      ]
+    ])->getBody();
+    $obj = json_decode($body);
+    //signin
+    if($obj->state=="failure")
+      return($obj->description);
+    elseif ($obj->state="success"){
+      $client = new Client();
+      $body = $client->post('http://localhost:3000/authentication/login/renegade', [
+        'user' =>[
+          'username' => $request['username'],
+          'password' => $request['password']
+        ]
+      ])->getBody();
+      $obj = json_decode($body);
+      return($obj->message);
 
-	  }
-	     return redirect()->back();
+
     }
 
 
 
-    public function getLogout()
-    {
-      Auth::logout();
-      return redirect()->route('welcome');
-    }
+
+
+
+
+    return redirect()->route('dashboard');
+
+  }
+  public function isOn($interest){
+    if($interest=='on')
+    return 1;
+    else
+    return 0;
+  }
+  public function SignIn(Request $request)
+  {
+
+    $this->validate($request,[
+      'username' => 'required',
+      'password' => 'required',
+
+    ]);
+
+
+    $client = new Client();
+    $body = $client->post('http://localhost:3000/authentication/login/renegade', [
+      'user' =>[
+        'username' => $request['username'],
+        'password' => $request['password']
+      ]
+    ])->getBody();
+    $obj = json_decode($body);
+    return($obj->message);
+    return redirect()->back();
+  }
+
+
+
+  public function getLogout()
+  {
+    Auth::logout();
+    return redirect()->route('welcome');
+  }
 
   public function getAccount()
   {
@@ -136,125 +156,125 @@ class UserController extends Controller{
 
   public function redirectToFacebook()
   {
-      return Socialite::driver('facebook')->redirect();
+    return Socialite::driver('facebook')->redirect();
   }
 
   /**
-   * Obtain the user information from GitHub.
-   *
-   * @return \Illuminate\Http\Response
-   */
+  * Obtain the user information from GitHub.
+  *
+  * @return \Illuminate\Http\Response
+  */
   public function handleFacebookCallback()
   {
-      $userfb = Socialite::driver('facebook')->user();
-      // $userfb = Socialite::driver('facebook')->stateless()->user();
-      $user = new User;
-      $user->name =explode(" ",$userfb->name);
-      // $user->last_name=$userfb->name['lastname'];
-      $user->email = $userfb->email;
-      $user->gender='male';
-      $user->password="default";
-      $user->channel="channel1";
-      $user->username=" ";
-      $rout=route('social');
-      return redirect($rout)->with(['first'=>$user->name[0],'last'=>$user->name[1],'email'=>$user->email,'gender'=>$user->gender,'username'=>$user->username]);
+    $userfb = Socialite::driver('facebook')->user();
+    // $userfb = Socialite::driver('facebook')->stateless()->user();
+    $user = new User;
+    $user->name =explode(" ",$userfb->name);
+    // $user->last_name=$userfb->name['lastname'];
+    $user->email = $userfb->email;
+    $user->gender='male';
+    $user->password="default";
+    $user->channel="channel1";
+    $user->username=" ";
+    $rout=route('social');
+    return redirect($rout)->with(['first'=>$user->name[0],'last'=>$user->name[1],'email'=>$user->email,'gender'=>$user->gender,'username'=>$user->username]);
 
   }
   public function terms(){
     return view('terms');
- }
- public function privacy(){
-  return view('privacy');
-}
+  }
+  public function privacy(){
+    return view('privacy');
+  }
 
   public function redirectToGoogle()
   {
-      return Socialite::driver('google')->redirect();
+    return Socialite::driver('google')->redirect();
   }
 
   /**
-   * Obtain the user information from GitHub.
-   *
-   * @return \Illuminate\Http\Response
-   */
+  * Obtain the user information from GitHub.
+  *
+  * @return \Illuminate\Http\Response
+  */
   public function handleGoogleCallback()
   {
     // $userg = Socialite::driver('google')->user();
-      $userg = Socialite::driver('google')->stateless()->user();
-      $user = new User;
-      $user->first_name = $userg->user['name']['givenName'];
-      $user->last_name = $userg->user['name']['familyName'];
-      $user->email = $userg->email;
-      $user->gender='male';
-      $user->password="default";
-      $user->channel="channel1";
-      $user->username=" ";
-      $rout=route('social');
-      return redirect($rout)->with(['first'=>$user->first_name,'last'=>$user->last_name,'email'=>$user->email,'gender'=>$user->gender,'username'=>$user->username]);
+    $userg = Socialite::driver('google')->stateless()->user();
+    $user = new User;
+    $user->first_name = $userg->user['name']['givenName'];
+    $user->last_name = $userg->user['name']['familyName'];
+    $user->email = $userg->email;
+    $user->gender='male';
+    $user->password="default";
+    $user->channel="channel1";
+    $user->username=" ";
+    $rout=route('social');
+    return redirect($rout)->with(['first'=>$user->first_name,'last'=>$user->last_name,'email'=>$user->email,'gender'=>$user->gender,'username'=>$user->username]);
 
   }
-public function socialup(Request $request){
+  public function socialup(Request $request){
 
-  $this->validate($request,[
-    'email' => 'required|email|unique:users',
-    'first_name' => 'required|max:120',
-    'last_name' => 'required|max:120',
-    'username'=>'required|unique:users',
-    'password' => 'required|min:4',
-    'confirmpassword'=>'required_with:password|same:password|min:4',
-    'gender' => 'required',
-    'channel' => 'required',
+    $this->validate($request,[
+      'email' => 'required|email|unique:users',
+      'first_name' => 'required|max:120',
+      'last_name' => 'required|max:120',
+      'username'=>'required|unique:users',
+      'password' => 'required|min:4',
+      'confirmpassword'=>'required_with:password|same:password|min:4',
+      'gender' => 'required',
+      'channel' => 'required',
 
     ]);
     $user = new User;
-  $password=bcrypt($request['password']);
-  $interest=['i1'=>$request['i1']?1:-1,'i2'=>$request['i2']?1:-1,'i3'=>$request['i3']?1:-1,'i4'=>$request['i4']?1:-1,'i5'=>$request['i5']?1:-1];
-  // return $request['first_name'];
-  $user->email=$request['email'];
-  $user->password=$password;
-  $user->first_name=$request['first_name'];
-  $user->last_name=$request['last_name'];
-  $user->gender=$request['gender'];
-  $user->channel=$request['channel'];
-  $user->username=$request['username'];
-  $user->interest=serialize($interest);
-  if($user->gender='male')
+    $password=bcrypt($request['password']);
+    $interest=['i1'=>$request['i1']?1:-1,'i2'=>$request['i2']?1:-1,'i3'=>$request['i3']?1:-1,'i4'=>$request['i4']?1:-1,'i5'=>$request['i5']?1:-1];
+    // return $request['first_name'];
+    $user->email=$request['email'];
+    $user->password=$password;
+    $user->first_name=$request['first_name'];
+    $user->last_name=$request['last_name'];
+    $user->gender=$request['gender'];
+    $user->channel=$request['channel'];
+    $user->username=$request['username'];
+    $user->interest=serialize($interest);
+    if($user->gender='male')
     $user->avatar='/src/img/dummymale.jpg';
-  else
+    else
     $user->avatar='/src/img/dummyfemale.jpg';
-  $user->save();
-  Auth::login($user);
-  return redirect()->route('dashboard');
+    $user->save();
+    Auth::login($user);
+    return redirect()->route('dashboard');
 
-}
+  }
 
-public function social(Request $request){
+  public function social(Request $request){
 
-  return view('social',['first'=>$request['first'],'last'=>$request['last'],'email'=>$request['email'],'gender'=>$request['gender']]);
+    return view('social',['first'=>$request['first'],'last'=>$request['last'],'email'=>$request['email'],'gender'=>$request['gender']]);
 
 
 
-}
+  }
 
-public function googleSignIn(){
+  public function googleSignIn(){
 
-  return Socialite::driver('google')->redirect();
+    return Socialite::driver('google')->redirect();
 
-}
+  }
 
-public function googleSignInCallback()
-{
-  // $userg = Socialite::driver('google')->user();
+  public function googleSignInCallback()
+  {
+    // $userg = Socialite::driver('google')->user();
     $userg = Socialite::driver('google')->stateless()->user();
     if (Auth::attempt(['email'=> $userg->email])){
 
       return redirect()->route('dashboard');
 
-  }
-     return redirect()->back();
+    }
+    return redirect()->back();
 
-}
-public function myposts()
+  }
+  public function myposts()
   {
     $post=Auth::user()->posts();
     return view('myposts',['posts'=>$post]);
