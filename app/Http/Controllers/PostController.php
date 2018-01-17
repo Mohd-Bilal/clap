fe3f42630fdcb28ade8f987c3c04dc788bb41076
albin <padinju@gmail.com>
2018-01-17 20:52:21 +0530

    post time

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,114 +18,115 @@ class PostController extends Controller{
 
     $client = new Client(); //GuzzleHttp\Client
     $body = $client->request('POST', 'http://localhost:3000/private/information/feed', [
-     'headers' => [
-         'Authorization' => 'Bearer '.$value
-     ],
-     'debug' => false,
-     'json' =>[
-       'requested_number' =>10,
-       'offset' => 0
-     ]
+      'headers' => [
+        'Authorization' => 'Bearer '.$value
+      ],
+      'debug' => false,
+      'json' =>[
+        'requested_number' =>10,
+        'offset' => 0
+      ]
 
-    ])->getBody();
-    $obj = json_decode($body);
-    if($obj->state == "success" && $obj->description_slug == "success-feeds"){
+      ])->getBody();
+
+      $obj = json_decode($body);
+      if($obj->state == "success" && $obj->description_slug == "success-feeds"){
         return view('dashboard',['posts'=>$obj->data]);
+      }
+      else {
+        return view('dashboard',['posts'=>$obj->description_slug]);
+      }
     }
-    else {
-      return view('dashboard',['posts'=>$obj->description_slug]);
-    }
-  }
 
-  public function createPost(Request $request){
-    $post=new Post();
-    $passedtag=['t1'=>$request['t1'],'t2'=>$request['t2'],'t3'=>$request['t3'],'t4'=>$request['t4'],'t5'=>$request['t5']];
-    $tags=serialize($passedtag);
-    $message='Body field required';
-    if($request['body']){
-      $post->body=$request['body'];
-      $post->tags=$tags;
+    public function createPost(Request $request){
+      $post=new Post();
+      $passedtag=['t1'=>$request['t1'],'t2'=>$request['t2'],'t3'=>$request['t3'],'t4'=>$request['t4'],'t5'=>$request['t5']];
+      $tags=serialize($passedtag);
+      $message='Body field required';
+      if($request['body']){
+        $post->body=$request['body'];
+        $post->tags=$tags;
+        $this->validate($request,[
+          'body'=>'required|max:1000'
+        ]);
+        if($request->user()->posts()->save($post)){
+          $message='Post successfully created';
+        }
+      }
+
+      return response()->json(['message'=>$message,'body'=>$request['body']], 200);
+
+    }
+
+    public function getDeletePost($post_id)
+    {
+      $post = Post::where('id',$post_id) ->first();
+      if (Auth::user() != $post->user){
+        return redirect()->back();
+      }
+      $post->delete();
+      return redirect()->route('dashboard')->with(['message' => 'Successfully Deleted!']);
+    }
+
+    public function editPost(Request $request){
       $this->validate($request,[
-        'body'=>'required|max:1000'
+        'body'=>'required'
       ]);
-      if($request->user()->posts()->save($post)){
-        $message='Post successfully created';
-      }
-    }
-
-    return response()->json(['message'=>$message,'body'=>$request['body']], 200);
-
-  }
-
-  public function getDeletePost($post_id)
-  {
-    $post = Post::where('id',$post_id) ->first();
-    if (Auth::user() != $post->user){
-      return redirect()->back();
-    }
-    $post->delete();
-    return redirect()->route('dashboard')->with(['message' => 'Successfully Deleted!']);
-  }
-
-  public function editPost(Request $request){
-    $this->validate($request,[
-      'body'=>'required'
-    ]);
-    $post=Post::find($request['postId']);
-    $post->body=$request['body'];
-    $post->update();
-    return response()->json(['new_body'=>$post->body],200);
-
-  }
-
-  public function likecount($postid){
-    $postlike=DB::table('likes')->get()->where('post_id',$postid);
-    $lcount=0;
-    $dcount=0;
-    foreach($postlike as &$likepost){
-      if($likepost->like)
-      $lcount++;
-      else
-      $dcount++;
+      $post=Post::find($request['postId']);
+      $post->body=$request['body'];
+      $post->update();
+      return response()->json(['new_body'=>$post->body],200);
 
     }
-    return ['likes'=>$lcount,'dislikes'=>$dcount];
-  }
-  public function postLike(Request $request){
-    $count=0;
-    $update=false;
-    $postid=$request['postId'];
-    $islike=$request['isLike']==='true';
-    $post=Post::find($postid);
-    if(!$post){
-      return null;
-    }
-    $user=Auth::user();
-    $likes=$user->likes()->where('post_id',$postid)->first();
 
-    if($likes){
-      $alreadylike=$likes->like;
-      $update=true;
-      if($alreadylike==$islike){
-        $likes->delete();
+    public function likecount($postid){
+      $postlike=DB::table('likes')->get()->where('post_id',$postid);
+      $lcount=0;
+      $dcount=0;
+      foreach($postlike as &$likepost){
+        if($likepost->like)
+        $lcount++;
+        else
+        $dcount++;
 
       }
-    }else{
-      $likes=new Like();
+      return ['likes'=>$lcount,'dislikes'=>$dcount];
     }
-    $likes->like=$islike;
-    $likes->user_id=$user->id;
-    $likes->post_id=$postid;
-    if($update){
-      $likes->update();
-    }
-    else{
-      $likes->save();
-    }
-    $count=$this->likecount($postid);
+    public function postLike(Request $request){
+      $count=0;
+      $update=false;
+      $postid=$request['postId'];
+      $islike=$request['isLike']==='true';
+      $post=Post::find($postid);
+      if(!$post){
+        return null;
+      }
+      $user=Auth::user();
+      $likes=$user->likes()->where('post_id',$postid)->first();
 
-    return response()->json(['number'=>$count['likes'],'dislikes'=>$count['dislikes']]);
+      if($likes){
+        $alreadylike=$likes->like;
+        $update=true;
+        if($alreadylike==$islike){
+          $likes->delete();
+
+        }
+      }else{
+        $likes=new Like();
+      }
+      $likes->like=$islike;
+      $likes->user_id=$user->id;
+      $likes->post_id=$postid;
+      if($update){
+        $likes->update();
+      }
+      else{
+        $likes->save();
+      }
+      $count=$this->likecount($postid);
+
+      return response()->json(['number'=>$count['likes'],'dislikes'=>$count['dislikes']]);
+    }
+
+
   }
-
-
-}
